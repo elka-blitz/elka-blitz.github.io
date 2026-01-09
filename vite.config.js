263ca34813284358f8b6ce47e5d52004080cefd6
elka-blitz <122494314@umail.ucc.ie>
2026-01-09 20:34:27 +0000

    Update src filepath

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import glsl from "vite-plugin-glsl";
 
 export default {
   base: '/elka-blitz.github.io/',
-  root: "/", // Sources files (typically where index.html is)
+  root: "./src", // Sources files (typically where index.html is)
   publicDir: "../static/", // Path from "root" to static assets (files that are served as they are)
   server: {
     host: true, // Open to local network and display URL
