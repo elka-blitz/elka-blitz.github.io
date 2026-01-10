2e4f61d4eb452346ea387d0e891844effccfb964
maggie-fyp <122438014@umail.ucc.ie>
2026-01-10 16:27:28 +0000

    Add second spinning cube and edit HTML

--- src/index.js
+++ src/index.js
@@ -10,41 +10,59 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { Text } from 'troika-three-text';
 import { XR_BUTTONS } from 'gamepad-wrapper';
+import { createText } from 'three/addons/webxr/Text2D.js';
 import { gsap } from 'gsap';
 import { init } from './init.js';
 
 const floorGeometry = new THREE.PlaneGeometry(6, 6);
 const floorMaterial = new THREE.MeshStandardMaterial({ color: 'black' });
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-var cubeRotation = 0
+let cubeRotation = 0
 const cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 const cubeMaterial = new THREE.MeshStandardMaterial({ color: 0x89F336});
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+const cube2 = new THREE.Mesh(
+	new THREE.BoxGeometry(0.3, 0.3, 0.3),
+	new THREE.MeshStandardMaterial({ color: '#F54927' }),
+);
 
 function setupScene({ scene, camera, renderer, player, controllers }) {
 
 	floor.rotateX(-Math.PI / 2);
 	scene.add(floor);
 
-
-	
 	scene.add(cube);
-	cube.position.set(-2, 1, -1.5);
+	cube.position.set(0, 1, -1.5);
 	cube.rotateY((Math.PI / 4) + cubeRotation);
+
+	scene.add(cube2);
+	cube2.position.set(0, 2, -1.5);
+
 
 	const light = new THREE.PointLight(0x89F336, 20, 100, 0.1);
 	light.position.copy(cube.position);
 	scene.add(light);
+
+	const text = createText(
+		'Check out my spinning cubes!',
+		0.09,
+	);
+	text.position.set(0, 1.6, -1);
+	scene.add(text);
+
 }
 
 function onFrame(
 	delta,
 	time,
-	{ scene, camera, renderer, player, controllers }, 
+	{ scene, camera, renderer, player, controllers },
 ) {
 	cubeRotation += 2
 	cube.rotateX(0.01)
 	cube.rotateY (0.01)
+	cube2.rotateY(0.05)
+	cube2.rotateX(0.05)
+
 }
 
 init(setupScene, onFrame);
