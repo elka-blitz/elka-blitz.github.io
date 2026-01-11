bed454fa1927c234aefaf84477931bf51b879ed4
elka-blitz <122494314@umail.ucc.ie>
2026-01-11 16:13:48 +0000

    Fuse both sample codes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+import * as THREE from "three";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { TubePainter } from "three/examples/jsm/misc/TubePainter.js";
+import { XRButton } from "three/examples/jsm/webxr/XRButton.js";
+import { XRControllerModelFactory } from "three/examples/jsm/webxr/XRControllerModelFactory.js";
 
-import * as THREE from 'three';
+let camera, scene, renderer;
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
+let stylus;
+let painter1;
+let gamepad1;
+let isDrawing = false;
+let prevIsDrawing = false;
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { Text } from 'troika-three-text';
-import { XR_BUTTONS } from 'gamepad-wrapper';
-import { createText } from 'three/addons/webxr/Text2D.js';
-import { gsap } from 'gsap';
-import { init } from './init.js';
+const material = new THREE.MeshNormalMaterial({
+  flatShading: true,
+  side: THREE.DoubleSide,
+});
+
+const cursor = new THREE.Vector3();
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 const floorGeometry = new THREE.PlaneGeometry(6, 6);
 const floorMaterial = new THREE.MeshStandardMaterial({ color: 'black' });
@@ -26,10 +38,12 @@ const cube2 = new THREE.Mesh(
 	new THREE.MeshStandardMaterial({ color: '#F54927' }),
 );
 
-function setupScene({ scene, camera, renderer, player, controllers }) {
+init();
 
-	floor.rotateX(-Math.PI / 2);
-	scene.add(floor);
+function init() {
+	const canvas = document.querySelector("canvas.webgl");
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0x1f0091);
 
 	scene.add(cube);
 	cube.position.set(0, 1, -1.5);
@@ -38,26 +52,77 @@ function setupScene({ scene, camera, renderer, player, controllers }) {
 	scene.add(cube2);
 	cube2.position.set(0, 2, -1.5);
 
+	floor.rotateX(-Math.PI / 2);
+	scene.add(floor);
 
-	const light = new THREE.PointLight(0x89F336, 20, 100, 0.1);
-	light.position.copy(cube.position);
+	camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 50);
+	camera.position.set(0, 1.6, 3);
+
+	const dracoLoader = new DRACOLoader();
+	dracoLoader.setDecoderPath("/draco/");
+
+	const gltfLoader = new GLTFLoader();
+	gltfLoader.setDRACOLoader(dracoLoader);
+
+	const grid = new THREE.GridHelper(4, 1, 0x111111, 0x111111);
+	scene.add(grid);
+
+	scene.add(new THREE.HemisphereLight(0x888877, 0x777788, 3));
+
+	const light = new THREE.DirectionalLight(0xffffff, 1.5);
+	light.position.set(0, 4, 0);
 	scene.add(light);
 
-	const text = createText(
-		'Check out my cubes!!!!',
-		0.09,
-	);
-	text.position.set(0, 1.6, -1);
-	scene.add(text);
+	painter1 = new TubePainter();
+	painter1.mesh.material = material;
+	painter1.setSize(0.1);
 
-}
+	scene.add(painter1.mesh);
 
-function onFrame(
-	delta,
-	time,
-	{ scene, camera, renderer, player, controllers },
-) {
-	cubeRotation += 2
+	renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+	renderer.setPixelRatio(window.devicePixelRatio, 2);
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setAnimationLoop(animate);
+	renderer.xr.enabled = true;
+	document.body.appendChild(XRButton.createButton(renderer, { optionalFeatures: ["unbounded"] }));
+
+	const controllerModelFactory = new XRControllerModelFactory();
+
+	controller1 = renderer.xr.getController(0);
+	controller1.addEventListener("connected", onControllerConnected);
+	controller1.addEventListener("selectstart", onSelectStart);
+	controller1.addEventListener("selectend", onSelectEnd);
+	controllerGrip1 = renderer.xr.getControllerGrip(0);
+	controllerGrip1.add(controllerModelFactory.createControllerModel(controllerGrip1));
+	scene.add(controllerGrip1);
+	scene.add(controller1);
+
+	controller2 = renderer.xr.getController(1);
+	controller2.addEventListener("connected", onControllerConnected);
+	controller2.addEventListener("selectstart", onSelectStart);
+	controller2.addEventListener("selectend", onSelectEnd);
+	controllerGrip2 = renderer.xr.getControllerGrip(1);
+	controllerGrip2.add(controllerModelFactory.createControllerModel(controllerGrip2));
+	scene.add(controllerGrip2);
+	scene.add(controller2);
+	}
+
+	window.addEventListener("resize", () => {
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+function onFrame() {
+	
 	cube.rotateX(0.01)
 	cube.rotateY (0.01)
 	cube2.rotateY(0.05)
@@ -65,4 +130,68 @@ function onFrame(
 
 }
 
-init(setupScene, onFrame);
+function animate() {
+  if (gamepad1) {
+    prevIsDrawing = isDrawing;
+    isDrawing = gamepad1.buttons[5].value > 0;
+    // debugGamepad(gamepad1);
+
+    if (isDrawing && !prevIsDrawing) {
+      const painter = stylus.userData.painter;
+      painter.moveTo(stylus.position);
+    }
+  }
+
+  handleDrawing(stylus);
+
+  // Render
+  onFrame();
+  renderer.render(scene, camera);
+}
+
+function handleDrawing(controller) {
+  if (!controller) return;
+
+  const userData = controller.userData;
+  const painter = userData.painter;
+
+  if (gamepad1) {
+    cursor.set(stylus.position.x, stylus.position.y, stylus.position.z);
+
+    if (userData.isSelecting || isDrawing) {
+      painter.lineTo(cursor);
+      painter.update();
+    }
+  }
+}
+
+function onControllerConnected(e) {
+  if (e.data.profiles.includes("logitech-mx-ink")) {
+    stylus = e.target;
+    stylus.userData.painter = painter1;
+    gamepad1 = e.data.gamepad;
+  }
+}
+
+function onSelectStart(e) {
+  if (e.target !== stylus) return;
+  const painter = stylus.userData.painter;
+  painter.moveTo(stylus.position);
+  this.userData.isSelecting = true;
+}
+
+function onSelectEnd() {
+  this.userData.isSelecting = false;
+}
+
+function debugGamepad(gamepad) {
+  gamepad.buttons.forEach((btn, index) => {
+    if (btn.pressed) {
+      console.log(`BTN ${index} - Pressed: ${btn.pressed} - Touched: ${btn.touched} - Value: ${btn.value}`);
+    }
+
+    if (btn.touched) {
+      console.log(`BTN ${index} - Pressed: ${btn.pressed} - Touched: ${btn.touched} - Value: ${btn.value}`);
+    }
+  });
+}
--- src/init.js
+++ src/init.js
@@ -16,8 +16,30 @@ import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
 
+let camera, scene, renderer;
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
+let stylus;
+let painter1;
+let gamepad1;
+let isDrawing = false;
+let prevIsDrawing = false;
+
+const material = new THREE.MeshNormalMaterial({
+  flatShading: true,
+  side: THREE.DoubleSide,
+});
+
+const cursor = new THREE.Vector3();
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 export async function init(setupScene = () => {}, onFrame = () => {}) {
 	// iwer setup
+	const canvas = document.querySelector("canvas.webgl");
 	let nativeWebXRSupport = false;
 	if (navigator.xr) {
 		nativeWebXRSupport = await navigator.xr.isSessionSupported('immersive-vr');
@@ -49,7 +71,7 @@ export async function init(setupScene = () => {}, onFrame = () => {}) {
 	document.body.appendChild(container);
 
 	const scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x808080);
+	scene.background = new THREE.Color(0x178787);
 
 	const camera = new THREE.PerspectiveCamera(
 		50,
@@ -63,10 +85,11 @@ export async function init(setupScene = () => {}, onFrame = () => {}) {
 	controls.target.set(0, 1.6, 0);
 	controls.update();
 
-	const renderer = new THREE.WebGLRenderer({ antialias: true });
+	const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.xr.enabled = true;
+	renderer.setAnimationLoop(animate);
 	container.appendChild(renderer.domElement);
 
 	const environment = new RoomEnvironment(renderer);
@@ -78,36 +101,23 @@ export async function init(setupScene = () => {}, onFrame = () => {}) {
 	player.add(camera);
 
 	const controllerModelFactory = new XRControllerModelFactory();
-	const controllers = {
-		left: null,
-		right: null,
-	};
-	for (let i = 0; i < 2; i++) {
-		const raySpace = renderer.xr.getController(i);
-		const gripSpace = renderer.xr.getControllerGrip(i);
-		const mesh = controllerModelFactory.createControllerModel(gripSpace);
-		gripSpace.add(mesh);
-		player.add(raySpace, gripSpace);
-		raySpace.visible = false;
-		gripSpace.visible = false;
-		gripSpace.addEventListener('connected', (e) => {
-			raySpace.visible = true;
-			gripSpace.visible = true;
-			const handedness = e.data.handedness;
-			controllers[handedness] = {
-				raySpace,
-				gripSpace,
-				mesh,
-				gamepad: new GamepadWrapper(e.data.gamepad),
-			};
-		});
-		gripSpace.addEventListener('disconnected', (e) => {
-			raySpace.visible = false;
-			gripSpace.visible = false;
-			const handedness = e.data.handedness;
-			controllers[handedness] = null;
-		});
-	}
+	controller1 = renderer.xr.getController(0);
+	controller1.addEventListener("connected", onControllerConnected);
+	controller1.addEventListener("selectstart", onSelectStart);
+	controller1.addEventListener("selectend", onSelectEnd);
+	controllerGrip1 = renderer.xr.getControllerGrip(0);
+	controllerGrip1.add(controllerModelFactory.createControllerModel(controllerGrip1));
+	scene.add(controllerGrip1);
+	scene.add(controller1);
+
+	controller2 = renderer.xr.getController(1);
+	controller2.addEventListener("connected", onControllerConnected);
+	controller2.addEventListener("selectstart", onSelectStart);
+	controller2.addEventListener("selectend", onSelectEnd);
+	controllerGrip2 = renderer.xr.getControllerGrip(1);
+	controllerGrip2.add(controllerModelFactory.createControllerModel(controllerGrip2));
+	scene.add(controllerGrip2);
+	scene.add(controller2);
 
 	function onWindowResize() {
 		camera.aspect = window.innerWidth / window.innerHeight;
@@ -123,25 +133,76 @@ export async function init(setupScene = () => {}, onFrame = () => {}) {
 		camera,
 		renderer,
 		player,
-		controllers,
 	};
 
 	setupScene(globals);
 
-	const clock = new THREE.Clock();
 	function animate() {
-		const delta = clock.getDelta();
-		const time = clock.getElapsedTime();
-		Object.values(controllers).forEach((controller) => {
-			if (controller?.gamepad) {
-				controller.gamepad.update();
+
+		if (gamepad1) {
+			prevIsDrawing = isDrawing;
+			isDrawing = gamepad1.buttons[5].value > 0;
+			// debugGamepad(gamepad1);
+
+			if (isDrawing && !prevIsDrawing) {
+			const painter = stylus.userData.painter;
+			painter.moveTo(stylus.position);
 			}
-		});
-		onFrame(delta, time, globals);
+		}
+
+		handleDrawing(stylus);	
+
 		renderer.render(scene, camera);
 	}
 
 	renderer.setAnimationLoop(animate);
 
 	document.body.appendChild(VRButton.createButton(renderer));
+}
+
+function handleDrawing(controller) {
+  if (!controller) return;
+
+  const userData = controller.userData;
+  const painter = userData.painter;
+
+  if (gamepad1) {
+    cursor.set(stylus.position.x, stylus.position.y, stylus.position.z);
+
+    if (userData.isSelecting || isDrawing) {
+      painter.lineTo(cursor);
+      painter.update();
+    }
+  }
+}
+
+function onControllerConnected(e) {
+  if (e.data.profiles.includes("logitech-mx-ink")) {
+    stylus = e.target;
+    stylus.userData.painter = painter1;
+    gamepad1 = e.data.gamepad;
+  }
+}
+
+function onSelectStart(e) {
+  if (e.target !== stylus) return;
+  const painter = stylus.userData.painter;
+  painter.moveTo(stylus.position);
+  this.userData.isSelecting = true;
+}
+
+function onSelectEnd() {
+  this.userData.isSelecting = false;
+}
+
+function debugGamepad(gamepad) {
+  gamepad.buttons.forEach((btn, index) => {
+    if (btn.pressed) {
+      console.log(`BTN ${index} - Pressed: ${btn.pressed} - Touched: ${btn.touched} - Value: ${btn.value}`);
+    }
+
+    if (btn.touched) {
+      console.log(`BTN ${index} - Pressed: ${btn.pressed} - Touched: ${btn.touched} - Value: ${btn.value}`);
+    }
+  });
 }
