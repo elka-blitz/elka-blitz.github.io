30d5093f97dc08ca1f8e510747d414bd62fb2ae5
elka-blitz <122494314@umail.ucc.ie>
2026-01-15 13:21:22 +0000

    Remove obsolete wrapper

--- src/controllerFunctions.js
+++ src/controllerFunctions.js
@@ -1,5 +1,3 @@
-let previous_button_map = []
-
 export const getController = (index, renderer, onControllerConnected, onSelectStart, onSelectEnd) => {
 	let controller = renderer.xr.getController(index);
 	controller.addEventListener('connected', onControllerConnected);
@@ -15,20 +13,3 @@ export const getControllerGrip = (index, renderer, controllerModelFactory) => {
 	);
 	return controllerGrip
 };
-
-// Wrapper for buttons, to differentiate press and release
-export const gamePadWrapper = (button_map, index_to_check_for_update) => {
-
-	const current_button_state = button_map
-
-        if (buttons[index_to_check_for_update].pressed && !prevButtons[index_to_check_for_update]) {
-          console.log(`Button ${index_to_check_for_updaten} pressed`);
-        }
-        if (!buttons[index_to_check_for_update].pressed && prevButtons[index_to_check_for_update]) {
-          console.log(`Button ${index_to_check_for_update} released`);
-        }
-	
-		previous_button_map = current_button_state
-
-	return button_frame_diff_state
-}
