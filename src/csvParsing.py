da07343c1c41a4dbde6968c876cfede062dcd3ef
elka-blitz <122494314@umail.ucc.ie>
2026-02-10 10:44:30 +0000

    Refactor CSV parsing script to improve structure and readability

--- src/csvParsing.py
+++ src/csvParsing.py
@@ -1,7 +1,8 @@
 import json
-import datetime
+import datetime                
 import sys
-
+import csv
+  
 # Pass file to convert to csv as CLI argument
 
 filename = sys.argv[1]
@@ -21,3 +22,22 @@ for i in data:
     print(i['t'])
     print(utc_date_time)
     print('')
+
+# Convert to CSV file
+fout = open(sys.argv[1][:-3] + 'csv', 'w', newline='')
+writer = csv.writer(fout)
+
+data_to_write = [['time', 'x', 'y', 'z']]
+
+for location in data:
+    timestamp = location['t']
+    x = location['s']['x']
+    y = location['s']['y']
+    z = location['s']['z']
+    
+    line = [timestamp, x, y, z]
+
+    data_to_write.append(line)
+
+writer.writerows(data_to_write)
+
