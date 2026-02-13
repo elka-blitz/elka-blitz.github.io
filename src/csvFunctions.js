7ff435a18eb1ea525bc07e095b3279c5baf4e69e
elka-blitz <122494314@umail.ucc.ie>
2026-02-13 16:04:29 +0000

    Refactor downloadCSV function to textDownload and update filename format

--- src/csvFunctions.js
+++ src/csvFunctions.js
@@ -1,21 +1,11 @@
-// export const csvMaker = (data) => {
-// 	const rows = [];
-// 	const headers = Object.keys(data);
-// 	rows.push(headers.join(','));
-
-// 	const values = Object.values(data).join(',');
-// 	rows.push(values);
-
-// 	return rows.join('\n');
-// };
-
-export const downloadCSV = (data) => {
+export const textDownload = (data, title) => {
+	// Feed in stringified JSON data
 	const blob = new Blob([data], { type: 'text/plain' });
 	const url = URL.createObjectURL(blob);
 	const link = document.createElement('a');
 
 	link.href = url;
 	var date = new Date();
-	link.download = `${date.getDate()}_${date.getMonth()}_${date.getFullYear()}_${date.getTime()}.txt`
+	link.download = `title_${date.getDate()}_${date.getMonth()}_${date.getFullYear()}_${date.getTime()}.txt`
 	link.click();
 };
