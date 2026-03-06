744f6d4c8e601d6025aa2039c9a7da4f3e6a2168
maggie-fyp <122438014@umail.ucc.ie>
2026-03-06 17:33:56 +0000

    Flip drawings for results

--- src/index.js
+++ src/index.js
@@ -1279,6 +1279,7 @@ const ShowResultsMode = () => {
 		svgPaintsArray[i].mesh.position.y = obj.position.y;
 		svgPaintsArray[i].mesh.position.z -= 0.05;
 		svgPaintsArray[i].mesh.rotateX(Math.PI); // flip each because they're upside down for some reason
+		svgPaintsArray[i].mesh.rotateY(Math.PI); // flip each because they're flipped as well
 		svgPaintsArray[i].mesh.visible = true;
 	});
 
