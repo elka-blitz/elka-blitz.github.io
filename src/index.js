a59799f5f290832f689b557d65f84bb2e4360a67
elka-blitz <122494314@umail.ucc.ie>
2026-03-09 22:18:08 +0000

    Add logging and accuracy class

--- src/index.js
+++ src/index.js
@@ -783,6 +783,7 @@ function animate(time, frame) {
 			isDrawing = gamepad1.buttons[5].value > 0;
 
 			if (isDrawing && !prevIsDrawing) {
+				event_logger.logEventData('press_front_button');
 				const painter = stylus?.userData.painter;
 				painter.moveTo(stylusPos);
 			}
