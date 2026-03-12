4bddeeff2d487dde811de8a255c74db8ebc8b0bb
elka-blitz <122494314@umail.ucc.ie>
2026-03-12 12:37:31 +0000

    Set condition

--- src/experimentConfig.js
+++ src/experimentConfig.js
@@ -1,6 +1,6 @@
 // true: 5deg surface, false: 85 deg surface in air
 // MARK: Change surface type
-export const isHorizontalSurface = true;
+export const isHorizontalSurface = false;
 
 const controllerType = {
     stylus: {
@@ -24,13 +24,13 @@ const controllerType = {
 }
 
 // MARK: Change controller type
-export const controllerObj = controllerType.stylus;
+export const controllerObj = controllerType.r_controller;
 
 // task will follow order of this array
 export const taskOrder = [
+    {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},
     {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},
     {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},
-    {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},
 ]
 
 export const degreesObj = {
