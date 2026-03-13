40ee4cea110d1492a748a8f1977de4fedbcbd775
maggie-fyp <122438014@umail.ucc.ie>
2026-03-13 09:59:04 +0000

    stylus vertical 321

--- src/experimentConfig.js
+++ src/experimentConfig.js
@@ -24,13 +24,13 @@ const controllerType = {
 }
 
 // MARK: Change controller type
-export const controllerObj = controllerType.l_controller;
+export const controllerObj = controllerType.stylus;
 
 // task will follow order of this array
 export const taskOrder = [
+    {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},
     {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},
     {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},
-    {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},
 ]
 
 export const degreesObj = {
