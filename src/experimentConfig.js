2b9f8e4a82e8f8606b45deccb86e402d8cbdf381
maggie-fyp <122438014@umail.ucc.ie>
2026-03-13 15:41:52 +0000

    stylus horizontal 123

--- src/experimentConfig.js
+++ src/experimentConfig.js
@@ -28,9 +28,9 @@ export const controllerObj = controllerType.stylus;
 
 // task will follow order of this array
 export const taskOrder = [
+    {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},
     {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},
     {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},
-    {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},
 ]
 
 export const degreesObj = {
