e51fae5dbe44bee4b0e1d75a8869033b9edff980
maggie-fyp <122438014@umail.ucc.ie>
2026-03-13 10:11:58 +0000

    rController vertical 312

--- src/experimentConfig.js
+++ src/experimentConfig.js
@@ -24,13 +24,13 @@ const controllerType = {
 }
 
 // MARK: Change controller type
-export const controllerObj = controllerType.l_controller;
+export const controllerObj = controllerType.r_controller;
 
 // task will follow order of this array
 export const taskOrder = [
-    {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},
-    {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},
     {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},
+    {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},
+    {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},
 ]
 
 export const degreesObj = {
