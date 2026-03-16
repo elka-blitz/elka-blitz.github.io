7358aac908e0f82b62c7c83c2be31cad137fe72c
maggie-fyp <122438014@umail.ucc.ie>
2026-03-16 16:25:29 +0000

    move nice work panel

--- src/uiElement.js
+++ src/uiElement.js
@@ -263,7 +263,7 @@ export class ResultsUI {
         ];
 
         this.containerArray.forEach((container) => {
-            container.position.set(0, 1.1, -1);
+            container.position.set(0, 1.5, -1.5);
             container.visible = false;
             scene.add(container);
         })
