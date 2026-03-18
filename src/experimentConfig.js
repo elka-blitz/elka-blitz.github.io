cdc684007da979c6dbd70ff24b18a92228d5448d
maggie-fyp <122438014@umail.ucc.ie>
2026-03-18 09:36:35 +0000

    stylus horizontal 312

--- src/experimentConfig.js
+++ src/experimentConfig.js
@@ -1,6 +1,6 @@
 // true: 5deg surface, false: 85 deg surface in air
 // MARK: Change surface type
-export const isHorizontalSurface = false;
+export const isHorizontalSurface = true;
 
 const controllerType = {
     stylus: {
@@ -24,13 +24,13 @@ const controllerType = {
 }
 
 // MARK: Change controller type
-export const controllerObj = controllerType.r_controller;
+export const controllerObj = controllerType.stylus;
 
 // task will follow order of this array
 export const taskOrder = [
+    {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},               // 3
     {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},   // 1
     {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},          // 2
-    {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},               // 3
 ]
 
 export const degreesObj = {
