a2cc0d11bcac63a3bff29e20cab4196daa1ea851
maggie-fyp <122438014@umail.ucc.ie>
2026-03-19 09:35:11 +0000

    controller vertical 321

--- src/experimentConfig.js
+++ src/experimentConfig.js
@@ -28,9 +28,9 @@ export const controllerObj = controllerType.r_controller;
 
 // task will follow order of this array
 export const taskOrder = [
-    {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},   // 1
-    {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},          // 2
     {name: "Cake", url: "assets/task3/task3.svg", frame: "assets/cakeFrame.png"},               // 3
+    {name: "Cup of Tea", url: "assets/task2/task2.svg", frame: "assets/cupFrame.png"},          // 2
+    {name: "Storefront", url: "assets/task1/task1.svg", frame: "assets/storefrontFrame.png"},   // 1
 ]
 
 export const degreesObj = {
