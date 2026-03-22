669156925fbc368abc7b1311bf508b472cd6a6fe
elka-blitz <122494314@umail.ucc.ie>
2026-03-22 09:01:45 +0000

    Add new parsing script

---
+++ data_processors/exportParser/zip_extractor_script/README.md
@@ -0,0 +1,3 @@
+Extractor script for zip downloaded from onedrive
+
+run main.py and pass zipfile path as argument in commandline
---
+++ data_processors/exportParser/zip_extractor_script/main.py
@@ -0,0 +1,7 @@
+from dataExtractor import dataExtractor
+import sys
+
+zip_archive = sys.argv[1] # Contains the filepath
+
+data_extractor = dataExtractor(zip_archive)
+
